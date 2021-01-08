35a628db7d09b507d6a7f32285a1c44f791da7dc
AldousWatts <charles.brough2@gmail.com>
2021-01-08 15:15:29 -0600

    fix issues, connect test project!

--- src/main/java/com/revature/FirebaseTest/controller/TestController.java
+++ src/main/java/com/revature/FirebaseTest/controller/TestController.java
@@ -1,14 +1,28 @@
 package com.revature.FirebaseTest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
 @RestController
+@CrossOrigin
 public class TestController {
 
-	@GetMapping("")
-	public String test() {
-		
+	@PostMapping("")
+	public String test(HttpServletRequest req ) {
+		try {
+			System.out.println(req.getHeader("auth"));
+			FirebaseAuth.getInstance().verifyIdToken(req.getHeader("auth"));
+		} catch (FirebaseAuthException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Failed!";
+		}
 		return "Success";
 	}
 }
