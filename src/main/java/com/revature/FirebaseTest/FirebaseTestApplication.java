6e30783cdfc3853370fb3360a2ce44aa415a1c6c
= <jadenwilson717@gmail.com>
2021-01-08 13:00:29 -0700

    initial commit

---
+++ src/main/java/com/revature/FirebaseTest/FirebaseTestApplication.java
@@ -0,0 +1,13 @@
+package com.revature.FirebaseTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/FirebaseTest/FirebaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.FirebaseTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
